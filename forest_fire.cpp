7e2c7a4af0699116653b6c6c62eec7e376b66ee1
Adanac AI <127537176+jess-heaton@users.noreply.github.com>
2025-03-11 14:47:18 +0000

    Update forest_fire.cpp

--- forest_fire.cpp
+++ forest_fire.cpp
@@ -54,7 +54,7 @@ void write_grid(const string& filename, const vector<int>& grid, int N) {
 void distribute_grid(int N, int iproc, int nproc, int &i0, int &i1) {
     int rows = N / nproc;
     i0 = iproc * rows;
-    // last rank takes any remainder
+    // Last rank takes any remainder
     if (iproc == nproc - 1)
         i1 = N;
     else
@@ -62,13 +62,13 @@ void distribute_grid(int N, int iproc, int nproc, int &i0, int &i1) {
 }
 
 // Perform the forest fire simulation
-void simulate_fire(int N, vector<int>& globalGrid, int iproc, int nproc, 
-                   int& steps, bool &fireReachedBottom) 
+void simulate_fire(int N, vector<int>& globalGrid, int iproc, int nproc,
+                   int& steps, bool &fireReachedBottom)
 {
     bool fireStillBurning = true;
     steps = 0;
     fireReachedBottom = false;
-    
+
     while (fireStillBurning) {
         // Determine which rows this process owns
         int i0, i1;
@@ -76,7 +76,6 @@ void simulate_fire(int N, vector<int>& globalGrid, int iproc, int nproc,
         int localRows = i1 - i0;
 
         // Build the local updated slice
-        // (reserve is fine, but resize is simpler to ensure data() is valid)
         vector<int> localNew(localRows * N, 0);
 
         // Update each cell in our slice
@@ -134,7 +133,7 @@ void simulate_fire(int N, vector<int>& globalGrid, int iproc, int nproc,
         // Perform the all-gatherv
         int localCount = localRows * N;
         MPI_Allgatherv(localNew.data(), localCount, MPI_INT,
-                       newGlobalGrid.data(), recvCounts.data(), displs.data(), 
+                       newGlobalGrid.data(), recvCounts.data(), displs.data(),
                        MPI_INT, MPI_COMM_WORLD);
 
         // Update global grid
@@ -152,8 +151,8 @@ void simulate_fire(int N, vector<int>& globalGrid, int iproc, int nproc,
             if (localBurning) break;
         }
         int globalBurning = 0;
-        // we use logical OR to see if any rank has burning
-        MPI_Allreduce(&localBurning, &globalBurning, 1, MPI_INT, MPI_LOR, MPI_COMM_WORLD);
+        // We use MPI_MAX to see if any rank has burning (1)
+        MPI_Allreduce(&localBurning, &globalBurning, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
         fireStillBurning = (globalBurning != 0);
 
         // Check if the fire reached the bottom row
@@ -167,7 +166,7 @@ void simulate_fire(int N, vector<int>& globalGrid, int iproc, int nproc,
             }
         }
         int globalBottom = 0;
-        MPI_Allreduce(&localBottom, &globalBottom, 1, MPI_INT, MPI_LOR, MPI_COMM_WORLD);
+        MPI_Allreduce(&localBottom, &globalBottom, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
         if (globalBottom)
             fireReachedBottom = true;
 
@@ -177,20 +176,20 @@ void simulate_fire(int N, vector<int>& globalGrid, int iproc, int nproc,
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
-    
+
     int iproc, nproc;
     MPI_Comm_rank(MPI_COMM_WORLD, &iproc);
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
-    
+
     // Expect at least 3 arguments: N, p, M
     if (argc < 4) {
         if (iproc == 0)
-            cerr << "Usage: " << argv[0] 
+            cerr << "Usage: " << argv[0]
                  << " N p M [input_filename] [output_filename]" << endl;
         MPI_Finalize();
         return 1;
     }
-    
+
     int N = atoi(argv[1]);
     double p = atof(argv[2]);
     int M = atoi(argv[3]);
@@ -198,12 +197,12 @@ int main(int argc, char** argv) {
     string outputFile = "final_grid.txt";
     if (argc >= 5) inputFile = argv[4];
     if (argc >= 6) outputFile = argv[5];
-    
+
     double t_start = MPI_Wtime();
-    
+
     int totalSteps = 0;
     int runsFireReachedBottom = 0;
-    
+
     // Perform M runs
     for (int run = 0; run < M; run++){
         vector<int> globalGrid;
@@ -226,7 +225,7 @@ int main(int argc, char** argv) {
         else {
             // Generate random grid on root
             if (iproc == 0) {
-                srand(time(NULL) + run); 
+                srand(time(NULL) + run);
                 generate_random_grid(N, p, globalGrid);
 
                 // Ignite top row
@@ -243,35 +242,35 @@ int main(int argc, char** argv) {
             // broadcast the grid
             MPI_Bcast(globalGrid.data(), N*N, MPI_INT, 0, MPI_COMM_WORLD);
         }
-        
+
         // Run the simulation
         int steps = 0;
         bool fireReachedBottom = false;
         simulate_fire(N, globalGrid, iproc, nproc, steps, fireReachedBottom);
-        
+
         // Collect results
         totalSteps += steps;
         if (fireReachedBottom) {
             runsFireReachedBottom++;
         }
-        
+
         // For the last run, root writes out final grid
         if (run == M - 1 && iproc == 0) {
             write_grid(outputFile, globalGrid, N);
         }
     }
-    
+
     double t_finish = MPI_Wtime();
     double avgSteps = (double) totalSteps / M;
-    
+
     // Root prints final summary
     if (iproc == 0) {
         cout << "Average number of steps: " << avgSteps << endl;
-        cout << "Fraction of runs where fire reached bottom: " 
+        cout << "Fraction of runs where fire reached bottom: "
              << (double) runsFireReachedBottom / M << endl;
         cout << "Total simulation time: " << (t_finish - t_start) << " seconds" << endl;
     }
-    
+
     MPI_Finalize();
     return 0;
 }
